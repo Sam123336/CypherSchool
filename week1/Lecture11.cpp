0c8e501f19d886055f724769f81cf72a4482f801
Sam Coder <115978232+Sam123336@users.noreply.github.com>
2024-06-28 12:51:36 +0530

    Create Lecture11.cpp

---
+++ week1/Lecture11.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i,input;
+	
+
+	for(i=1;i<101;i++)
+	{
+		cin>>input;
+		if(input==65) 
+		{
+			cout<<"Congrats You have guessed correct";
+			break;
+		}
+	}
+
+	
+}
