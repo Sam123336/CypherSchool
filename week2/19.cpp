b44b8461aca4ff299816a4116e90150e9f8f3949
Sam Coder <115978232+Sam123336@users.noreply.github.com>
2024-07-22 20:37:58 +0530

    Create 19.cpp

---
+++ week2/19.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long int a;
+    int b;
+
+    cout<<"The size of Int is: "<<sizeof(b)<<endl;
+    cout<<"The size of Long Long Int is: "<<sizeof(a)<<endl;
+    return 0;
+}
