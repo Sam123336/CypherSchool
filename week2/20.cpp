744509e2156220ce770fca79e1a2d26ef6682cf3
Sam Coder <115978232+Sam123336@users.noreply.github.com>
2024-07-22 20:40:12 +0530

    Create 20.cpp

---
+++ week2/20.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long int a;
+    int b;
+
+    cout<<"The size of Int is: "<<sizeof(b)<<endl;
+    cout<<"The size of Long Long Int is: "<<sizeof(a)<<endl;
+
+    cout<<"The address of Int is: "<<&b<<endl;
+    cout<<"The address of Long Long Int is: "<<&a<<endl;
+    return 0;
+}
+
